87343bf3fbd90bd040d542705ccb3a74450a5af5
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-02-28 21:44:22 -0500

    firebase-testrun

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { auth } from "./firebase/init";
+import { auth, db } from "./firebase/init";
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc } from "firebase/firestore"
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -12,13 +13,62 @@ function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
 
+  async function updatePost() {
+    const hardcodedId = "WAadXat9zbGmnH8e7Kej";
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId);
+    console.log(post);
+    const newPost = {
+     ...post,
+      title: "Land a $1m job"
+    };
+    console.log(newPost);
+    updateDoc(postRef, newPost);
+  }
+
+ function deletePost() {
+  const hardcodedId = "WAadXat9zbGmnH8e7Kej";
+  const postRef = doc(db, "posts", hardcodedId);
+  deleteDoc(postRef);
+ }
+
+ function createPost() {
+  const post = {
+    title: "Finish Interview Section",
+    description: "Do Frontend Simplified",
+    uid: user.uid,
+  };
+   addDoc(collection(db, "posts"), post);
+ }
+
+ async function getAllPosts() {
+   const { docs } = await getDocs(collection(db, "posts"));
+   const posts = docs.map(elem => ({...elem.data(), id: elem.id }));
+   console.log(posts);
+ }
+
+ async function getPostById(id) {
+   const postRef = doc(db, "posts", id);
+   const postSnap = await getDoc(postRef);
+   return postSnap.data();
+ }
+
+ async function getPostByUid() {
+   const postCollectionRef = await query(
+     collection(db, "posts"),
+     where("uid", "==", user.uid)
+   );
+   const { docs } = await getDocs(postCollectionRef);
+   console.log(docs.map(doc => doc.data()));
+ }
+
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setTimeout(() => {
         setLoading(false);
       }, 2000);
       if (user) {
-        setUser(user.email[0].toUpperCase());
+        setUser(user);
       }
     });
   }, []);
@@ -37,8 +87,8 @@ function App() {
   function login() {
     signInWithEmailAndPassword(auth, "email@email.com", "test123")
       .then(({ user }) => {
-        setUser(user.email[0].toUpperCase());
-        console.log(user);
+        setUser(user);
+        console.log(user.email[0].toUpperCase());
       })
       .catch((error) => {
         console.log(error.message);
@@ -86,9 +136,15 @@ function App() {
         </>
       ) : (
         <button className="logoutbtn" onClick={logout}>
-          <span>E</span>
-        </button>
+          {user.email}
+        </button> 
       )}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </nav>
   );
 }
